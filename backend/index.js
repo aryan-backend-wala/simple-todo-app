0e19d006a636db2f46d623b75daceed414414c38
Aryan Verma <aryan.computerscience@gmail.com>
2024-10-22 10:30:18 +0530

    integrated dotenv package

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import todosRouter from './routes/todos.js'
 import connectDB from './database/index.js';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
